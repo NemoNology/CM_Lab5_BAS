e95d87ac941cc7a34746cb891d6d3bd2fbe58564
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-10-28 17:49:28 +0900

    V0.7
    V0.7: доработки по алгоритму, но ещё не известно, что делать с вектором Y и где его взять...

--- CM_Lab5_BAS.cpp
+++ CM_Lab5_BAS.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <iomanip>
+#include <exception>
 
 
 using namespace std;
@@ -13,7 +14,7 @@ void Show_2D_Matrix(const vector<vector<double>>& a) {
         cout << "\t";
         for (unsigned short j = 0; j < a[i].size(); j++) {
 
-            cout << setw(10) << setprecision(8) << a[i][j];
+            cout << setw(16) << setprecision(8) << a[i][j];
 
         }
         cout << endl;
@@ -47,64 +48,26 @@ void Zeroing_2D_Matrix(vector<vector<double>>& a) {
 }
 
 
-// Функция для проверки 0 в a[i][i]
-short int Zero_Check_aii(const vector<vector<double>>& a) {
-
-    for (unsigned short i = 0; i < a.size(); i++) {
-
-        if (a[i][i] == 0) {
-
-            return i;
-
-        }
-    }
-
-    return -1;
-}
-
-
-// Функция для проверки 0 в a[0][0]
-bool Zero_Check_a00(const vector<vector<double>>& a) {
-
-    if (a[0][0] == 0) {
-
-        return 1;
-
-    }
-
-    return 0;
-}
-
-// Функция для перестановки строк, при условии, что a[i][i] (На которое мы делим) = 0 
-void Swith_Lines_aii(vector<vector<double>>& a, const short int sign) {
-
-
-
-}
-
-
 // Функция для перестановки строк, при условии, что a[0][0] (На которое мы делим) = 0 
-void Swith_Lines_a00(vector<vector<double>>& a, bool sign) {
+void Swith_Lines_a00(vector<vector<double>>& a) {
 
 
-    if (sign == 1) {
+    static unsigned short i = 1; // Создаём статическую переменную для хранения индекса строки, с которой будем менять a[0] 
 
-        for (unsigned short i = 0; i < a.size(); i++) {
+    if (i >= a.size()) { // При превышении размерности массива выбрасываем ошибку о неправильной матрице
 
-            if (a[i][0] != 0) {
-
-                vector<double> temp(a[0].size());
-
-                temp = a[0];
-                a[0] = a[i];
-                a[i] = temp;
-
-            }
-
-        }
+        throw invalid_argument("Матрица не та...");
 
     }
 
+    vector<double> temp(a.size());
+
+
+    temp = a[0];
+    a[0] = a[i];
+    a[i] = temp;
+
+    i++; // Увеличиваем i на единицу, чтобы при повторном вызове данного метода менять местами a[0] уже со следующей строкой
 }
 
 // Метод Халецкого;
@@ -114,14 +77,15 @@ void Method(const vector<vector<double>>& a) {
     // Создание 1Д\2Д матриц для записи B & T; Y матриц, а также для записи результатов (х)
     vector<double> x(a.size());
     vector<double> y(a.size());
+    vector<double> с(a.size());
     vector<vector<double>> b = a;
     vector<vector<double>> t = a;
 
 
     // Обнуляем матрицы B & T
-    Zeroing_2D_Matrix(b); 
+    Zeroing_2D_Matrix(b);
     Zeroing_2D_Matrix(t);
-    
+
 
     // Вычисляем элементы матриц B & T
 
@@ -130,21 +94,66 @@ void Method(const vector<vector<double>>& a) {
 
         b[i][0] = a[i][0];
 
-        if (i >= 1) {
+        for (unsigned short j = 1; j < t.size(); j++) {
 
-            t[0][i] = a[0][i] / b[0][0];
+            t[0][j] = a[0][j] / b[0][0];
+
+            /*for (unsigned short k = 1; k < a.size(); k++) {
+                for (unsigned short m = 0; m < k; m++) {
+
+                    b[i][k] = 
+
+                }
+            }*/
 
         }
 
+        /*cout << "\n\tStep 1:\n\t\tB:\t(" + i  << ")";
+        Show_2D_Matrix(b);
+        cout << "\n\t\tT:\t(" + i << ")";
+        Show_2D_Matrix(t);*/
+
     }
-    
+
+    for (unsigned short k = 1; k < a.size(); k++) {
+        for (unsigned short i = k; i < a.size(); i++) {               // Просто иду по алгоритму в примере...
+
+            b[i][k] = a[i][k];
+
+            for (unsigned short m = 0; m < k; m++) {
+
+                b[i][k] -= b[i][m] * t[m][k];
+
+            }
+
+        }
+
+        if (k < a.size() - 1) {
+
+            for (unsigned short j = k + 1; j < a.size(); j++) {        // Просто иду по алгоритму в примере...
+
+                t[k][j] = a[k][j];
+
+                for (unsigned short m = 0; m < k; m++) {
+
+                    t[k][j] -= b[k][m] * t[m][j];
+
+                }
+
+                t[k][j] /= b[k][k];
+
+            }
+        }
+    }
+
+
 
 }
 
 
 int main()
 {
-    
+
 
 
     // Мой массив
@@ -155,22 +164,36 @@ int main()
     {-0.67, 0.17, -0.86, 0.88, -0.88, 0.53},
     {0.14, -0.11, 0.86, -0.61, 0.61, 0.32},
     {0.83, -0.97, -0.29, -0.66, 0.7, -0.88},
-    {0.91, -0.04, -0.41, -0.24, 0.09, 0.66} };  
-    
+    {0.91, -0.04, -0.41, -0.24, 0.09, 0.66} };
+
     // ##############################################################
 
-    
+
     // Вызов метода
     cout << "\nInitial matrix:";
     Show_2D_Matrix(mat);
 
-    // Проверка на a[0][0] = 0
-    Swith_Lines_a00(mat, Zero_Check_a00(mat)); 
+    // Проверка на a[0][0] = 0 // Не в методе, ибо там const принимаемая матрица
+    try {
+
+        while (mat[0][0] == 0) {
+
+            Swith_Lines_a00(mat);
+
+        }
+    }
+
+    catch (invalid_argument e) { // Ошибка
+
+        cout << "\n\tВходная матрица не соответствует условиям метода!\n\ta[0][0] = 0!\n\n";
+        return 0;
+
+    }
 
     // Вывод результатов решения
-    cout << "\nFinal response matrix";   
+    cout << "\nFinal response matrix";
     Method(mat);
-    
+
     system("pause"); // Окончание программы
     return 0;
 }
